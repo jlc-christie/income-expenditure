377c9280d6dc978975327903d201c520b2633c1f
James Christie <jlc_christie@live.com>
2021-02-10 15:38:55 +0000

    Added app to installed apps and applied migrations

--- income_expenditure_proj/income_expenditure/tests.py
+++ income_expenditure_proj/income_expenditure/tests.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import IEStatement, Person, Address
 
-# Create your tests here.
+
+class IEStatementTestCase(TestCase):
+    def setUp(self):
+        address = Address.objects.create(
+            paon='1',
+            street='Example Street',
+            town_city='London',
+            county='London',
+            postcode='SW12EQ'
+        )
+        user = User.objects.create_user('fred', 'jlc_christie@live.com', 'abadpassword')
+        user.first_name = 'Fred'
+        user.last_name = 'Ives'
+        user.save()
+        person = Person.objects.create(user=user, address=address)
+        statement = IEStatement.objects.create(
+            person=person,
+            salary=2500,
+            income_other=500,
+            mortgage=600,
+            rent=0,
+            utilities=200,
+            travel=200,
+            food=300,
+            loans=500,
+            credit_cards=300,
+            expenditure_other=250
+        )
+
+    def test_income(self):
+        statement = IEStatement.objects.get(id=1)
+        self.assertEqual(statement.income, 3000)
+
