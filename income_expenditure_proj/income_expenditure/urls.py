5184c7353a032347667dbb9210167eee94a6c1d1
James Christie <jlc_christie@live.com>
2021-02-11 17:26:35 +0000

    Created initial Statement List View

--- income_expenditure_proj/income_expenditure/urls.py
+++ income_expenditure_proj/income_expenditure/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import IEStatementView, CreatePersonView
+from .views import IEStatementCreate, CreatePersonView, IEStatementList
 
 app_name = 'income_expenditure'
 urlpatterns = [
-    path('statements/new', IEStatementView.as_view(), name='new_statement'),
+    path('statements', IEStatementList.as_view(), name='statements'),
+    path('statements/new', IEStatementCreate.as_view(), name='new_statement'),
     path('accounts/create', CreatePersonView.as_view(), name='account_create'),
 ]
--- income_expenditure_proj/income_expenditure/views.py
+++ income_expenditure_proj/income_expenditure/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.urls import reverse_lazy
+from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.generic import FormView
+from django.views.generic import FormView, ListView
 
 from .forms import IEStatementForm, PersonForm
+from .models import IEStatement
 
 
 class CreatePersonView(FormView):
@@ -22,10 +24,10 @@ class CreatePersonView(FormView):
         return super().form_valid(form)
 
 
-class IEStatementView(LoginRequiredMixin, FormView):
+class IEStatementCreate(LoginRequiredMixin, FormView):
     template_name = 'income_expenditure/new_statement.html'
     form_class = IEStatementForm
-    success_url = reverse_lazy('new_statement')
+    success_url = reverse_lazy('income_expenditure:new_statement')
 
     def form_valid(self, form):
         statement = form.save(commit=False)
@@ -34,3 +36,13 @@ class IEStatementView(LoginRequiredMixin, FormView):
         return super().form_valid(form)
 
 
+class IEStatementList(ListView):
+    template_name = 'income_expenditure/statement_list.html'
+    model = IEStatement
+    paginate_by = 10
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated and hasattr(self.request.user, 'person'):
+            return IEStatement.objects.filter(person=self.request.user.person)
+        else:
+            return IEStatement.objects.none()
